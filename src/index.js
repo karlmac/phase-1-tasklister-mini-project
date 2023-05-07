9423fe2521cae91d66cc6645aa455d09a94cdf8d
Karlmac Yerima <yerimakarlmac@gmail.com>
2023-05-07 06:18:44 -0500

    edits index.js and style.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //document.getElementById("")
+  
+  const ulElement = document.getElementById("tasks");
+  let index = 0;
+  
+  document.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const txtValue = document.getElementById("new-task-description").value;
+    
+    const li = document.createElement("li");
+    li.textContent = txtValue;
+    index++;
+    li.id = "li" + index;
+    li.append(createDelBtn(li.id));
+    
+    if(txtValue == null || txtValue == "") {
+      alert("Please enter a valid task"); return;
+    }
+    ulElement.appendChild(li);
+    
+    document.getElementById("new-task-description").value = "";
+
+    //console.log(ulElement);
+  });
+  
+  function createDelBtn(remId){
+    const btn = document.createElement("button");
+    btn.textContent = "X";
+    btn.addEventListener("click", () => document.getElementById(remId).remove() );
+    return btn;
+  }
+  
 });
